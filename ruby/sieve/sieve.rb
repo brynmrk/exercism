8a48dbd293f937f65c5b48ffd88c502e0f029446
Bryan Mark Fajutag <fbryanmark@gmail.com>
2017-03-07 01:42:40 +0000

    fix indentation

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -10,8 +10,8 @@ class Sieve
   def primes
     list_of_primes = []
     (2..@num).each do |num|
-        list_of_primes.any? { |l| num % l == 0 } ? nil : list_of_primes.push(num)
-      end
+      list_of_primes.any? { |l| num % l == 0 } ? nil : list_of_primes.push(num)
+    end
     list_of_primes.length > 0 ? list_of_primes : []
   end
 end
