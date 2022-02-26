c02bfbf9f96664ae080ca9997cc03bec2ce64e4c
Bryan Mark Fajutag <19178579+brynmrk@users.noreply.github.com>
2022-02-27 01:57:50 +0800

    updated solution to run-length-encoding

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,24 +1,26 @@
 class RunLengthEncoding
   def self.encode data
-    data_length = data.length
+    tmp_d = data.dup
+    data_length = tmp_d.length
     result = ""
     limit, cnt, pos = 0, 0, 0
     while limit < data_length do
       pos = 0
-      checker = data[pos]
-      count = data.chars.take_while{|letter| letter == checker}.count
+      checker = tmp_d[pos]
+      count = tmp_d.chars.take_while{|letter| letter == checker}.count
       result += count > 1 ? "#{count}#{checker}" : "#{checker}"
       limit += count
-      data.slice!(0..count-1)
+      tmp_d.slice!(0..count-1)
     end
     result
   end
-  
+
   def self.decode data
+    tmp_d = data.dup
     result = ""
-    while data.length > 0 do
-      num = data.slice!(/\d*/)
-      result += num.length > 0 ? data.slice!(0) * num.to_i : data.slice!(0)
+    while tmp_d.length > 0 do
+      num = tmp_d.slice!(/\d*/)
+      result += num.length > 0 ? tmp_d.slice!(0) * num.to_i : tmp_d.slice!(0)
     end
     result
   end
