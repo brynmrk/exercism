9808fe81144eba19b9e0651e41a3b55db819794a
Bryan Mark Fajutag <19178579+brynmrk@users.noreply.github.com>
2022-02-27 05:21:05 +0800

    added solution to pig-latin

---
+++ ruby/pig-latin/pig_latin.rb
@@ -0,0 +1,17 @@
+class PigLatin
+  def self.translate(phrase)
+    phrase.split.map(&method(:pig_word)).join(' ')
+  end
+  def self.pig_word(word)
+    if @vowel_sounds.any? { |s| word.start_with?(s) }
+      "#{word}ay"
+    elsif starter = @consonant_sets.select { |s| word.start_with?(s) }.first
+      "#{word.slice(starter.length..word.length)}#{starter}ay"
+    else
+      "#{word.slice(1..word.length)}#{word.slice(0,1)}ay"
+    end
+  end
+
+  @vowel_sounds   = %w(a e i o u yt xr)
+  @consonant_sets = %w(ch thr th sch qu squ rh)
+end
