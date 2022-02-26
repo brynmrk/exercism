0243a571b16b8a393b684f5b3a9f7852159c8a9a
Bryan Mark Fajutag <19178579+brynmrk@users.noreply.github.com>
2022-02-27 01:30:30 +0800

    updated solution to bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,6 +2,8 @@ class Bob
   def self.hey(remark)
     if remark.strip.empty?
       'Fine. Be that way!'
+    elsif remark == remark.upcase && remark != remark.downcase && remark.strip.end_with?('?')
+      'Calm down, I know what I\'m doing!'
     elsif remark == remark.upcase && remark != remark.downcase
       'Whoa, chill out!'
     elsif remark.strip.end_with?('?')
