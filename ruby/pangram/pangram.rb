1f6f49923fc7efc498ff82108f06a2e458376a16
Bryan Mark Fajutag <19178579+brynmrk@users.noreply.github.com>
2022-02-27 01:35:11 +0800

    updated solution to pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,10 +1,5 @@
-module BookKeeping
-  VERSION = 4
-end
-
 class Pangram
   def self.pangram?(phrase)
-    phrase.downcase
-    ('a'..'z').all? { |ltr| phrase.include? (ltr) }
+    ('a'..'z').all? { |ltr| phrase.downcase.include? (ltr) }
   end
 end
